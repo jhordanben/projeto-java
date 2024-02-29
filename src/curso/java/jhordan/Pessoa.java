0ef646e782da9f8be8209f1a2397073096c5a279
jhordanben <benaraujo74@gmail.com>
2024-02-29 14:52:32 -0300

    Alteração e criação da classe Pessoa

---
+++ src/curso/java/jhordan/Pessoa.java
@@ -0,0 +1,57 @@
+package curso.java.jhordan;
+
+public class Pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	private int idade;
+	
+	private Date dataNascimento;
+	
+	private Date dataNascimento2;
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Date getDataNascimento2() {
+		return dataNascimento2;
+	}
+
+	public void setDataNascimento2(Date dataNascimento2) {
+		this.dataNascimento2 = dataNascimento2;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+
+}
